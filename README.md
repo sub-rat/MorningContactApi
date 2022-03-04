07fe5c53e01bddd03d28ccfbc812f404ed028795
Subrat Gyawali <subrat.gyawali@gmail.com>
2022-03-04 21:31:53 +0545

    gin setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MorningContactApi
+
+url = http://localhost:8080
+
+
+GET     /contacts  get list of contacts
+POST    /contacts  create new contact
+
+GET     /contacts/:id  get list of contacts
+POST    /contacts/:id  create new contact
+PUT     /contacts/:id    update contact
+DELETE  /contacts/:id    delete contact
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Contact Api")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/test", Test)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func Test(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello Test",
+	})
 }
