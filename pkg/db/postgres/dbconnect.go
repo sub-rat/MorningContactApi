22321c5c9a1d8fc4c4a2f3a60466692fd2761a97
Subrat Gyawali <subrat.gyawali@gmail.com>
2022-03-18 20:58:12 +0545

    Revert "preload and env updated"

--- pkg/db/postgres/dbconnect.go
+++ pkg/db/postgres/dbconnect.go
@@ -1,18 +1,13 @@
 package postgres
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/sub-rat/MorningContactApi/internals/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+	dsn := "host=localhost user=postgres password=password dbname=MoringContactApi port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
